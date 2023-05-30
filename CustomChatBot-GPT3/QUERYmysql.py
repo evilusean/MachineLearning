95d51bac760d454004c034bac89838339473061d
evilusean <82889521+evilusean@users.noreply.github.com>
2023-05-30 11:19:40 -0300

    Update QUERYmysql.py

--- CustomChatBot-GPT3/QUERYmysql.py
+++ CustomChatBot-GPT3/QUERYmysql.py
@@ -6,13 +6,15 @@ load_dotenv()
 
 mydb = mysql.connector.connect(
   host = "cloud.mindsdb.com",
-  user = os.environ.get("USER'), #email used for mindsdb account
+  user = os.environ.get("USER"), #email used for mindsdb account
   password = os.environ.get("PASSWORD"), #password used for mindsdb
   port = "3306"
 )
 
 cursor = mydb.cursor()
+#type question into 'text = '
 cursor.execute('''SELECT response from mindsdb.snoopstein_model WHERE author_username = "mindsdb' AND text ="What is the QuestSean?"''')
 
+#you need to iterate over each character as a response
 for x in cursor:
   print(x)
